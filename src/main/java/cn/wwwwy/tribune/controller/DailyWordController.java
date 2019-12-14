079f7a5b94230fbaeab040a1a8e45600940fcf0c
wwwwy1 <840985277@qq.com>
2019-12-14 22:27:07 +0800

    每日一言功能还差前台显示，后台显示包括分页。还需完善更新与删除。

--- src/main/java/cn/wwwwy/tribune/controller/DailyWordController.java
+++ src/main/java/cn/wwwwy/tribune/controller/DailyWordController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * <p>
  *  前端控制器
@@ -24,10 +27,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class DailyWordController {
 	@Autowired
 	private IDailyWordService iDailyWordService;
-	@PostMapping
+	@PostMapping(value = "add")
 	public Result insertDailyWord(DailyWord dailyWord){
-		iDailyWordService.save(dailyWord);
+		if (dailyWord.getId()==null)
+			iDailyWordService.save(dailyWord);
+		else
+			iDailyWordService.updateById(dailyWord);
 		return new Result("成功",200,dailyWord);
 	}
-
+	@PostMapping(value = "deletes")
+	public Result deletes(String ids){
+		List<String> idList = Arrays.asList(ids.split(","));
+		boolean b = iDailyWordService.removeByIds(idList);
+		if (b){
+			return new Result("删除成功",200,null);
+		}else {
+			return new Result("删除失败",500,null);
+		}
+	}
+	@PostMapping(value = "delete")
+	public Result delete(Integer id){
+		boolean b = iDailyWordService.removeById(id);
+		if (b){
+			return new Result("删除成功",200,null);
+		}else {
+			return new Result("删除失败",500,null);
+		}
+	}
 }
