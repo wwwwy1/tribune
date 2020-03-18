14dc415430b030fcc3f6732f35761955ef3f651e
wwwwy1 <840985277@qq.com>
2020-03-18 16:00:50 +0800

    image cropper测试服务

--- src/test/java/cn/wwwwy/tribune/TribuneApplicationTests.java
+++ src/test/java/cn/wwwwy/tribune/TribuneApplicationTests.java
@@ -1,9 +1,13 @@
 package cn.wwwwy.tribune;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @SpringBootTest
 class TribuneApplicationTests {
